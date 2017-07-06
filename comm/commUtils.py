01e454ed53117bc3f318df55b0a69aa3b3519654
zhangby <zhangby@wisea.com.cn>
2017-07-06 14:19:32 +0800

    修改 commUtils 方法增加 sql格式化

--- comm/commUtils.py
+++ comm/commUtils.py
@@ -4,15 +4,50 @@ import uuid
 from datetime import *
 import random
 
+#格式化sql
+def formatSql(sql,param):
+    params = []
+    sq = sql.split('?')
+    for p in param.split(','):
+        p = p.lstrip()
+        params.append("'"+(p.split('(')[0])+"'" if p != 'null' else 'null')
+    for i in range(len(params)):
+        sq[i] = sq[i]+params[i]
+    return ''.join(sq)
 #json 数据格式化
-def jsonFormat(dic1):
+def formatJson(dic1):
 	json_dic2=json.dumps(dic1,sort_keys=False,indent=4,separators=(',',':'),ensure_ascii=False )
 	print(json_dic2)
 
 #excel 数据转json
 def excelToJson(adr):
     excel = read_excel(adr,True)
-    jsonFormat(formatExcel(0,excel,0))
+    formatJson(formatExcel(0,excel,0))
+#json 数据转excel
+def jsonToExcel(adr):
+    js = pd.read_json(adr);
+    for n in js.index:
+        print('===================')
+        print(n)
+        print("-------------------")
+        for e in js.ix[n]['result']: 
+            if(type(js.ix[n]['result']) == dict):       
+                if(type(js.ix[n]['result'][e])==list):
+                    print("-------------------")
+                    print(e)
+                    print("-=-=-=-=-=-=-=-=-=-")
+                    each_json(js.ix[n]['result'][e][0])
+                    print("-------------------")
+                elif(type(js.ix[n]['result'][e])==dict):
+                    print("-------------------")
+                    print(e)
+                    print("-=-=-=-=-=-=-=-=-=-")
+                    each_json(js.ix[n]['result'][e],"    ")
+                    print("-------------------")
+                else:
+                    print(e)
+           
+        print('===================')
 
 #读取excel 返回 dataFrame
 def read_excel(adr,flag=False):
@@ -160,3 +195,28 @@ def preJson(name):
     elif(name.find('flag')>=0 or name.find('status')>=0 or name.find('type')>=0):
         rs = random.sample(list(range(0,2)),1)[0] # 在 0,1 中随意选择
     return rs
+
+def formatUrl(url):
+    json_ = {}
+    if(url.find('?') >= 0):
+        arr = url.split('?')[1].split('&')
+        for i in arr:
+            j_ = i.split('=')
+            json_[j_[0]] = j_[1]
+    return formatJson(json_)
+
+def each_json(jsdata):
+    for j in jsdata:
+        if(type(jsdata[j]) == list):
+            print("-------------------")
+            print(j)
+            print("-=-=-=-=-=-=-=-=-=-")
+            each_json(jsdata[j][0])
+            print("-------------------")
+        elif(type(jsdata[j]) == dict):
+            print("-------------------")
+            print(j)
+            print("-=-=-=-=-=-=-=-=-=-")
+            each_json(jsdata[j])
+            print("-------------------")
+        print(str(j))
